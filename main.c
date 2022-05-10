be9e7959297f56abca26dea05aec7b7c79dc74d1
Tyler Tibbets <zaydentibbets@gmail.com>
2022-05-09 22:25:05 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+// My first function
+void myfunc(void)
+{
+    printf("This is my first function");
+}
+// Hello world
 int main(int argc, char** argv)
 {
+    myfunc();
     printf("Hello World!");
     return 0;
 }
